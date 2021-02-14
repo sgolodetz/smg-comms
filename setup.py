cc17d450c4aad657216221a9133b3f83cc595cbe
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-14 20:52:29 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="smg-comms",
+    version="0.0.1",
+    author="Stuart Golodetz",
+    author_email="stuart.golodetz@cs.ox.ac.uk",
+    description="Client-server architecture for remote mapping",
+    long_description="",  #long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgolodetz/smg-comms",
+    packages=find_packages(include=["smg.comms", "smg.comms.*"]),
+    include_package_data=True,
+    install_requires=[
+        "smg-utility"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
