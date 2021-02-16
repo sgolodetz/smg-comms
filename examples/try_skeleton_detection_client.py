403010cd843099d10c6dd5cb8a7a235db2bed117
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-16 14:39:46 +0000

    Send a real image across.

--- examples/try_skeleton_detection_client.py
+++ examples/try_skeleton_detection_client.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 from smg.comms.skeletons import SkeletonDetectionClient
@@ -6,7 +7,7 @@ from smg.comms.skeletons import SkeletonDetectionClient
 def main() -> None:
     with SkeletonDetectionClient() as client:
         frame_idx: int = 0
-        image: np.ndarray = np.full((480, 640, 3), (255, 0, 0), dtype=np.uint8)
+        image: np.ndarray = cv2.imread("C:/smglib/smg-lcrnet/smg/external/lcrnet/058017637.jpg")
         world_from_camera: np.ndarray = np.eye(4)
         if client.begin_detection(frame_idx, image, world_from_camera):
             print(client.end_detection(frame_idx))
