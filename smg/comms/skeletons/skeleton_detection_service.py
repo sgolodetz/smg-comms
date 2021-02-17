c7e75a52a7f1313f9c97a95bce891d9625a72724
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-17 15:51:53 +0000

    More tidying and commenting.

--- smg/comms/skeletons/skeleton_detection_service.py
+++ smg/comms/skeletons/skeleton_detection_service.py
@@ -21,9 +21,9 @@ class SkeletonDetectionService:
         """
         Construct a skeleton detection service.
 
-        :param frame_processor:     TODO
+        :param frame_processor:     The function to use to detect skeletons in frames.
         :param port:                The port on which the service should listen for connections.
-        :param frame_decompressor:  TODO
+        :param frame_decompressor:  An optional function to use to decompress received frames.
         """
         self.__frame_decompressor: Optional[Callable[[FrameMessage], FrameMessage]] = frame_decompressor
         self.__frame_processor: Callable[[np.ndarray, np.ndarray], List[Skeleton]] = frame_processor
@@ -41,67 +41,78 @@ class SkeletonDetectionService:
         print(f"Listening for connections on 127.0.0.1:{self.__port}...")
 
         client_sock: Optional[socket.SocketType] = None
+
+        while client_sock is None:
+            timeout: float = 0.1
+            readable, _, _ = select([server_sock], [], [], timeout)
+
+            for s in readable:
+                if s is server_sock:
+                    client_sock, client_endpoint = server_sock.accept()
+                    print(f"Accepted connection from client @ {client_endpoint}")
+
+        # Once a client has connected, process any detection requests received from it.
         connection_ok: bool = True
         receiver: RGBDFrameReceiver = RGBDFrameReceiver()
         skeletons: Optional[List[Skeleton]] = None
 
         while connection_ok:
-            if client_sock is not None:
-                control_msg: SkeletonControlMessage = SkeletonControlMessage()
-                connection_ok = SocketUtil.read_message(client_sock, control_msg)
-                if connection_ok:
-                    value: int = control_msg.extract_value()
-                    if value == SkeletonControlMessage.BEGIN_DETECTION:
-                        print("Begin Detection")
+            # First, try to read a control message from the client.
+            control_msg: SkeletonControlMessage = SkeletonControlMessage()
+            connection_ok = SocketUtil.read_message(client_sock, control_msg)
 
-                        # Try to read a frame header message.
-                        max_images: int = 2
-                        header_msg: FrameHeaderMessage = FrameHeaderMessage(max_images)
-                        connection_ok = SocketUtil.read_message(client_sock, header_msg)
+            # If that succeeds:
+            if connection_ok:
+                # Extract the control message value to tell us whether this is the start or end of a detection.
+                value: int = control_msg.extract_value()
+
+                # If this is the start of a detection:
+                if value == SkeletonControlMessage.BEGIN_DETECTION:
+                    print("Begin Detection")
+
+                    # Try to read a frame header message.
+                    max_images: int = 2
+                    header_msg: FrameHeaderMessage = FrameHeaderMessage(max_images)
+                    connection_ok = SocketUtil.read_message(client_sock, header_msg)
+
+                    # If that succeeds:
+                    if connection_ok:
+                        # Set up a frame message accordingly.
+                        image_shapes: List[Tuple[int, int, int]] = header_msg.get_image_shapes()
+                        image_byte_sizes: List[int] = header_msg.get_image_byte_sizes()
+                        frame_msg: FrameMessage = FrameMessage(image_shapes, image_byte_sizes)
+
+                        # Try to read the contents of the frame message from the client.
+                        connection_ok = SocketUtil.read_message(client_sock, frame_msg)
 
                         # If that succeeds:
                         if connection_ok:
-                            # Set up a frame message accordingly.
-                            image_shapes: List[Tuple[int, int, int]] = header_msg.get_image_shapes()
-                            image_byte_sizes: List[int] = header_msg.get_image_byte_sizes()
-                            frame_msg: FrameMessage = FrameMessage(image_shapes, image_byte_sizes)
+                            # Send an acknowledgement to the client.
+                            connection_ok = SocketUtil.write_message(client_sock, AckMessage())
 
-                            # Try to read the contents of the frame message from the client.
-                            connection_ok = SocketUtil.read_message(client_sock, frame_msg)
+                            # Decompress the frame as necessary.
+                            decompressed_frame_msg: FrameMessage = frame_msg
+                            if self.__frame_decompressor is not None:
+                                decompressed_frame_msg = self.__frame_decompressor(frame_msg)
 
-                            # If that succeeds:
-                            if connection_ok:
-                                # Send an acknowledgement to the client.
-                                connection_ok = SocketUtil.write_message(client_sock, AckMessage())
+                            # Detect any people who are present in the frame.
+                            receiver(decompressed_frame_msg)
 
-                                # Decompress the frame as necessary.
-                                decompressed_frame_msg: FrameMessage = frame_msg
-                                if self.__frame_decompressor is not None:
-                                    decompressed_frame_msg = self.__frame_decompressor(frame_msg)
+                            skeletons = self.__frame_processor(
+                                receiver.get_rgb_image(), receiver.get_depth_image(), receiver.get_pose()
+                            )
 
-                                # TODO: Comment here.
-                                receiver(decompressed_frame_msg)
+                # Otherwise, if this is the end of a detection:
+                else:
+                    # TODO: Get rid of the "blocking" nonsense.
+                    frame_idx, blocking = np.abs(value) - 1, value >= 0
+                    print(f"End Detection: Frame Index = {frame_idx}, blocking = {blocking}")
 
-                                skeletons = self.__frame_processor(
-                                    receiver.get_rgb_image(), receiver.get_depth_image(), receiver.get_pose()
-                                )
-                    else:
-                        frame_idx, blocking = np.abs(value) - 1, value >= 0
-                        print(f"End Detection: Frame Index = {frame_idx}, blocking = {blocking}")
+                    if skeletons is not None:
+                        data: np.ndarray = np.frombuffer(bytes(repr(skeletons), "utf-8"), dtype=np.uint8)
+                        data_msg: DataMessage = DataMessage(len(data))
+                        np.copyto(data_msg.get_data(), data)
 
-                        if skeletons is not None:
-                            data: np.ndarray = np.frombuffer(bytes(repr(skeletons), "utf-8"), dtype=np.uint8)
-                            data_msg: DataMessage = DataMessage(len(data))
-                            np.copyto(data_msg.get_data(), data)
-
-                            connection_ok = \
-                                SocketUtil.write_message(client_sock, SimpleMessage[int](len(data))) and \
-                                SocketUtil.write_message(client_sock, data_msg)
-            else:
-                timeout: float = 0.1
-                readable, _, _ = select([server_sock], [], [], timeout)
-
-                for s in readable:
-                    if s is server_sock:
-                        client_sock, client_endpoint = server_sock.accept()
-                        print(f"Accepted connection from client @ {client_endpoint}")
+                        connection_ok = \
+                            SocketUtil.write_message(client_sock, SimpleMessage[int](len(data))) and \
+                            SocketUtil.write_message(client_sock, data_msg)
