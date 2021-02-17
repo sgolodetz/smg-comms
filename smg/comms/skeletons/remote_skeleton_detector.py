01910c28f343a912a12c462bcb77385f72ac8c34
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-17 19:42:54 +0000

    More tidying and commenting.

--- smg/comms/skeletons/remote_skeleton_detector.py
+++ smg/comms/skeletons/remote_skeleton_detector.py
@@ -5,27 +5,37 @@ from typing import Callable, List, Optional, Tuple
 
 from smg.skeletons import Skeleton
 
-from ..base import AckMessage, DataMessage, FrameHeaderMessage, FrameMessage, RGBDFrameMessageUtil, \
-    SimpleMessage, SocketUtil
+from ..base import AckMessage, DataMessage, FrameHeaderMessage, FrameMessage, SimpleMessage
+from ..base import RGBDFrameMessageUtil, SocketUtil
 from .skeleton_control_message import SkeletonControlMessage
 
 
 class RemoteSkeletonDetector:
-    """TODO"""
+    """A skeleton detector that makes use of a remote skeleton detection service to operate."""
 
     # CONSTRUCTOR
 
-    def __init__(self, endpoint: Tuple[str, int] = ("127.0.0.1", 7852), *, timeout: int = 10,
+    def __init__(self, endpoint: Tuple[str, int] = ("127.0.0.1", 7852), *, timeout: Optional[float] = None,
                  frame_compressor: Optional[Callable[[FrameMessage], FrameMessage]] = None):
+        """
+        Construct a remote skeleton detector.
+
+        :param endpoint:            The service host and port, e.g. ("127.0.0.1", 7852).
+        :param timeout:             An optional socket timeout (in seconds).
+        :param frame_compressor:    An optional function to use to compress frames prior to transmission.
+        """
         self.__alive: bool = False
         self.__frame_compressor: Optional[Callable[[FrameMessage], FrameMessage]] = frame_compressor
 
         try:
+            # Try to connect to the service.
             self.__sock: socket.SocketType = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.__sock.connect(endpoint)
-            # self.__sock.settimeout(timeout)
+            if timeout is not None:
+                self.__sock.settimeout(timeout)
             self.__alive = True
         except ConnectionRefusedError:
+            # If we couldn't connect to the service, raise an exception.
             raise RuntimeError("Error: Could not connect to the service")
 
     # DESTRUCTOR
@@ -46,10 +56,21 @@ class RemoteSkeletonDetector:
 
     # PUBLIC METHODS
 
-    def begin_detection(self, frame_idx: int, image: np.ndarray, world_from_camera: np.ndarray) -> bool:
+    def begin_detection(self, image: np.ndarray, world_from_camera: np.ndarray) -> bool:
+        """
+        Try to request that the remote skeleton detection service detect any skeletons in the specified image.
+
+        .. note::
+            This will return False iff the connection drops before the request can be completed.
+
+        :param image:               The image.
+        :param world_from_camera:   The pose from which the image was captured.
+        :return:                    True, if the detection was successfully requested, or False otherwise.
+        """
         # Make the frame message.
+        dummy_frame_idx: int = -1
         frame_msg: FrameMessage = RGBDFrameMessageUtil.make_frame_message(
-            frame_idx, image, np.zeros(image.shape[:2], dtype=np.uint16), world_from_camera
+            dummy_frame_idx, image, np.zeros(image.shape[:2], dtype=np.uint16), world_from_camera
         )
 
         # If requested, compress the frame prior to transmission.
@@ -77,29 +98,47 @@ class RemoteSkeletonDetector:
 
         return connection_ok
 
-    def detect_skeletons(self, frame_idx: int, image: np.ndarray,
-                         world_from_camera: np.ndarray) -> Optional[List[Skeleton]]:
-        if self.begin_detection(frame_idx, image, world_from_camera):
+    def detect_skeletons(self, image: np.ndarray, world_from_camera: np.ndarray) -> Optional[List[Skeleton]]:
+        """
+        Try to use the remote skeleton detection service to detect any skeletons in the specified image.
+
+        :param image:               The image.
+        :param world_from_camera:   The pose from which the image was captured.
+        :return:                    A list of skeletons, if the detection succeeded, or None otherwise.
+        """
+        if self.begin_detection(image, world_from_camera):
             return self.end_detection()
         else:
             return None
 
     def end_detection(self) -> Optional[List[Skeleton]]:
+        """
+        Try to request that the remote skeleton detection service send across any skeletons that it has just detected.
+
+        :return:    The skeletons, if successful, or None otherwise.
+        """
+        # First send the end detection message, then read the size of the data that the service wants to send across.
         data_size_msg: SimpleMessage[int] = SimpleMessage[int]()
         connection_ok: bool = \
             SocketUtil.write_message(self.__sock, SkeletonControlMessage.end_detection()) and \
             SocketUtil.read_message(self.__sock, data_size_msg)
 
+        # If that succeeds:
         if connection_ok:
+            # Read the data itself.
             data_msg: DataMessage = DataMessage(data_size_msg.extract_value())
             connection_ok = SocketUtil.read_message(self.__sock, data_msg)
+
+            # If that succeeds:
             if connection_ok:
+                # Construct a list of skeletons from the data, and return it.
                 data: str = str(data_msg.get_data().tobytes(), "utf-8")
                 skeletons: List[Skeleton] = eval(
                     data, {'array': np.array, 'Keypoint': Skeleton.Keypoint, 'Skeleton': Skeleton}
                 )
                 return skeletons
 
+        # If anything goes wrong, return None.
         return None
 
     def terminate(self) -> None:
