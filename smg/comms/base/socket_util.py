bc81acb8f1395490808f3d513470a62edd94bef1
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-09-15 00:42:44 +0100

    Fix a termination bug.

--- smg/comms/base/socket_util.py
+++ smg/comms/base/socket_util.py
@@ -1,7 +1,8 @@
 import numpy as np
 import socket
+import threading
 
-from typing import TypeVar
+from typing import Optional, TypeVar
 
 from .message import Message
 
@@ -19,34 +20,42 @@ class SocketUtil:
     # PUBLIC STATIC METHODS
 
     @staticmethod
-    def read_message(sock: socket.SocketType, msg: T) -> bool:
+    def read_message(sock: socket.SocketType, msg: T, stop_waiting: Optional[threading.Event] = None) -> bool:
         """
         Attempt to read a message of type T from the specified socket.
 
-        :param sock:    The socket.
-        :param msg:     The T into which to copy the message, if reading succeeded.
-        :return:        True, if reading succeeded, or False otherwise.
+        :param sock:            The socket.
+        :param msg:             The T into which to copy the message, if reading succeeded.
+        :param stop_waiting:    An optional event that can be used to make the operation stop waiting if needed.
+        :return:                True, if reading succeeded, or False otherwise.
         """
         try:
             data = b""  # type: bytes
 
             # Until we've read the number of bytes we were expecting:
             while len(data) < msg.get_size():
-                # Try to get the remaining bytes.
-                received = sock.recv(msg.get_size() - len(data))  # type: bytes
+                try:
+                    # Try to get the remaining bytes.
+                    received = sock.recv(msg.get_size() - len(data))  # type: bytes
 
-                # If we made progress, append the new bytes to the buffer. If not, something's wrong, so return False.
-                if len(received) > 0:
-                    data += received
-                else:
-                    return False
+                    # If we made progress, append the new bytes to the buffer.
+                    if len(received) > 0:
+                        data += received
+
+                    # Otherwise, something's wrong, so return False.
+                    else:
+                        return False
+                except socket.timeout:
+                    # If a timeout occurs and we should stop waiting, return False, else loop round for another try.
+                    if stop_waiting is not None and stop_waiting.is_set():
+                        return False
 
             # If we managed to get the number of bytes were were expecting, copy the buffer into the output message
             # and return True to indicate a successful read.
             np.copyto(msg.get_data(), np.frombuffer(data, dtype=np.uint8))
             return True
-        except (ConnectionAbortedError, ConnectionResetError, socket.timeout, ValueError):
-            # If any exceptions are thrown during the read, return False.
+        except (ConnectionAbortedError, ConnectionResetError, ValueError):
+            # If any (non-timeout) exceptions are thrown during the read, return False.
             return False
 
     @staticmethod
